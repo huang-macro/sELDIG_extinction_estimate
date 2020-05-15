d69d913f6136a169a8d431781215aa71be7954e8
shuang <shuang@shuang-mbp.local>
2020-05-15 16:56:32 +0200

    add r script to read trees

---
+++ R_scripts/read_trees.R
@@ -0,0 +1,14 @@
+### Read trees from Oskar
+
+# install.packages('ape',repos='http://cran.uni-muenster.de/')
+
+library(ape)
+
+setwd("~/Dropbox/sDiv_working_group/sELDIG_extinction_estimate")
+
+# trees in a list
+trees<-readRDS('data/Hagen_phylogenies.rds')
+
+# corresponding parameters in a dataframe
+paras<-readRDS('data/Hagen_parameters.rds')
+
