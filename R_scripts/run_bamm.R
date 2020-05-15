b4b87301d59ace09ac18fc10a47496b7b2ed5f6a
shuang <shuang@shuang-mbp.local>
2020-05-16 01:35:37 +0200

    install BAMMtools in R

---
+++ R_scripts/run_bamm.R
@@ -0,0 +1,9 @@
+### Run BAMM by Dan Rabosky (http://bamm-project.org)
+
+install.packages('BAMMtools',repos='http://cran.uni-muenster.de/')
+
+library(BAMMtools)
+
+setwd("~/Dropbox/sDiv_working_group/sELDIG_extinction_estimate")
+
+source("R_scripts/read_trees.R")
