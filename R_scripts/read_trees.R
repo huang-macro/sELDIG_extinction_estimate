d2d78052e262f0a40dda27d079a75611288be4e9
shuang <shuang@shuang-mbp.local>
2020-05-16 00:44:25 +0200

    find phylo objects in the tree list

--- R_scripts/read_trees.R
+++ R_scripts/read_trees.R
@@ -12,5 +12,10 @@ trees<-readRDS('data/Hagen_phylogenies.rds')
 # corresponding parameters in a dataframe
 # paras<-readRDS('data/Hagen_parameters.rds')
 
-i<-1
-i.tree<-trees[[i]]
+# Not all items in the tree list are trees
+is.tree<-c()
+for (i in 1:length(trees)){
+	if (class(trees[[i]]) == "phylo") is.tree<-c(is.tree, i)
+}
+
+phylos<-trees[is.tree]
