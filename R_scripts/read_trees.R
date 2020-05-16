7ea9b5182b4812795271845d8c0798e67bd8f38c
shuang <shuang@shuang-mbp.local>
2020-05-16 16:26:43 +0200

    Select good trees for BAMM
    BAMM can only analyze trees that are ultrametric and fully resolved, with
    no zero-length branch. I added 0.01 to all zero-length branches in these trees.
    In addition, I only selected trees with at least 50 extant species.

--- R_scripts/read_trees.R
+++ R_scripts/read_trees.R
@@ -1,8 +1,9 @@
 ### Read trees from Oskar
 
-# install.packages('ape',repos='http://cran.uni-muenster.de/')
+# install.packages('geiger',repos='http://cran.uni-muenster.de/')
 
 library(ape)
+library(geiger)
 
 setwd("~/Dropbox/sDiv_working_group/sELDIG_extinction_estimate")
 
@@ -15,7 +16,24 @@ trees<-readRDS('data/Hagen_phylogenies.rds')
 # Not all items in the tree list are trees
 is.tree<-c()
 for (i in 1:length(trees)){
-	if (class(trees[[i]]) == "phylo") is.tree<-c(is.tree, i)
+	if (class(trees[[i]]) == "phylo") 
 }
 
 phylos<-trees[is.tree]
+
+###########################################################################
+###########################################################################
+###########################################################################
+###########################################################################
+###########################################################################
+## BAMM only takes ultrametric, fully bifurcating trees with all 
+## branch lengths >0
+## Here, I only select trees with >= 50 extant species
+
+for (i in 1:length(phylos)){
+	p<-drop.extinct(phylos[[i]])
+	if (length(p$tip.label)>50){
+		p$edge.length[which(p$edge.length==0)]<-0.01
+		write.tree(p, paste("bamm_build/good_trees/tree_", i, ".tre", sep=''))
+	}
+}
